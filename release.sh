6e713b240ac4acf203a25332525d87a1a9197bf9
Pahaz White <pahaz.white@gmail.com>
2018-04-01 15:20:32 +0300

    release.sh: init

---
+++ release.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+git checkout master
+git pull
+
+die() { echo "$*" 1>&2 ; exit 1; }
+
+python -m pip install pip>=9.0.3
+python -m pip install setuptools>=39.0.1
+python -m pip install twine>=1.11.0
+python -m pip install wheel>=0.30.0
+python -m pip install pygments>=2.2.0
+
+VERSION=$(python setup.py --version)
+NAME=$(python setup.py --name)
+
+LAST_VERSION=$(git describe --tags --abbrev=0 --match v*)
+LAST_VERSION=${LAST_VERSION:1}
+
+if [[ ${LAST_VERSION} == ${VERSION} ]]; then
+	die "You should update release VERSION"
+fi
+
+echo "PACKAGE: $NAME"
+echo "CURRENT VERSION: $LAST_VERSION"
+echo "NEW VERSION: $VERSION"
+
+python setup.py build sdist
+python -m twine upload dist/*
+git tag -a v$VERSION -m "version $VERSION"
