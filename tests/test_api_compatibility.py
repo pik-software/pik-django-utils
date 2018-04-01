eeacb96145cd11430db9d664a6f6a05ffecff5ec
Pahaz White <pahaz.white@gmail.com>
2018-04-01 15:50:27 +0300

    tests: init

---
+++ tests/test_api_compatibility.py
@@ -0,0 +1,15 @@
+import pytest
+
+@pytest.fixture(params=[
+    'core', 'libs'
+])
+def pik_module(request):
+    return request.param
+
+
+def test_import_pik_namespace():
+	assert __import__('pik')
+
+
+def test_import_pik_packages(pik_module):
+	assert __import__(f'pik.{pik_module}')
