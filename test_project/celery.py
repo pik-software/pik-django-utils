a99c597a60dac5b3b14b9043f7ea2eb0a4633ad7
Pahaz White <pahaz.white@gmail.com>
2018-05-27 01:00:07 +0300

    test_project: init

---
+++ test_project/celery.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import, unicode_literals
+import os
+import celery
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_project.settings')
+
+app = celery.Celery('test_project')  # noqa: pylint=invalid-name
+
+# Using a string here means the worker don't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self: celery.Task):
+    return 'Request: {0!r}'.format(self.request.task)
