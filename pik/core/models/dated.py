60ae4b5df3c3bc82fa0d59412e6e334c930cac91
martbln <stepinilya@gmail.com>
2018-10-26 12:08:01 +0300

    update verbose name for created updated fields

--- pik/core/models/dated.py
+++ pik/core/models/dated.py
@@ -4,10 +4,10 @@ from django.utils.translation import ugettext_lazy as _
 
 class Dated(models.Model):
     created = models.DateTimeField(
-        editable=False, auto_now_add=True, verbose_name=_('Created')
+        editable=False, auto_now_add=True, verbose_name=_('created')
     )
     updated = models.DateTimeField(
-        editable=False, auto_now=True, verbose_name=_('Updated')
+        editable=False, auto_now=True, verbose_name=_('updated')
     )
 
     class Meta:
