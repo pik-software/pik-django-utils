009c9c85a58c246eaaf4e34b6ca05fd634ee292f
spice0xff <spice0xff@gmail.com>
2021-11-17 22:09:03 +0700

    bus.util clear

--- pik/legacy/bus/utils.py
+++ pik/legacy/bus/utils.py
@@ -10,7 +10,3 @@ def get_host():
         'frameworkVersion': django.get_version(),
         'operatingSystemVersion': f'{platform.system()} {platform.version()}'
     }
-
-
-def get_message_type(model_class):
-    return f'urn:message:{model_class.__module__}.{model_class.__name__}'
