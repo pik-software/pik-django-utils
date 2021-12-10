6c23369960ddf6a6da225549d151736ebfb84289
zenin.ds <zenin.ds@pik.ru>
2021-12-10 11:00:05 +0300

    fix qa

--- pik/api/serializers.py
+++ pik/api/serializers.py
@@ -85,7 +85,8 @@ class StandardizedProtocolSerializer(serializers.ModelSerializer):
             kwargs.pop('required')
 
         if extra_kwargs.get('read_only', kwargs.get('read_only', False)):
-            extra_kwargs.pop('required', None)  # Read only fields should always omit the 'required' argument.
+            # Read only fields should always omit the 'required' argument.
+            extra_kwargs.pop('required', None)
 
         kwargs.update(extra_kwargs)
 
@@ -158,10 +159,10 @@ class LabeledModelSerializerMixIn:
         if isinstance(parent, ModelSerializer):
             opts = parent.Meta.model._meta
             if not self._label_is_set:
-                self.label = _normalize_label(opts.get_field(self.source)
+                self.label = _normalize_label(opts.get_field(self.source)  # noqa: attribute-defined-outside-init
                                               .verbose_name)
             if not self._help_text_is_set:
-                self.help_text = opts.get_field(self.source).help_text
+                self.help_text = opts.get_field(self.source).help_text  # noqa: attribute-defined-outside-init
 
 
 class ValidatedModelSerializerMixIn:
