8b54b89c5ad6710106a59e79c7a702cecc27d99d
Evgenii Timofeev <timofeeves@pik.ru>
2021-12-23 11:41:06 +0300

    update clear_old_history

--- pik/core/clear_history/tasks.py
+++ pik/core/clear_history/tasks.py
@@ -1,12 +1,44 @@
+import logging
+import math
+
 from celery import app
 from django.conf import settings
-from simple_history.management.commands.clean_old_history import (
-    Command as CleanOldHistoryCommand)
+from django.utils import timezone
+from simple_history import models, utils
+from simple_history.exceptions import NotHistoricalModelError
+
+LOGGER = logging.getLogger(__name__)
+
+
+def _get_models():
+    to_process = set()
+    for model in models.registered_models.values():
+        try:  # avoid issues with multi-table inheritance
+            history_model = utils.get_history_model_for_model(model)
+        except NotHistoricalModelError:
+            continue
+        to_process.add((model, history_model))
+    return to_process
 
 
 @app.shared_task()
 def clear_history():
     """Deletes oldest rows from history models."""
     keep_days = getattr(settings, 'HISTORY_CLEANING_KEEP_DAYS', 180)
-    cleaner = CleanOldHistoryCommand()
-    cleaner.handle(auto=True, days=keep_days)
+    chunk_size = getattr(settings, 'HISTORY_CLEANING_CHUNK_SIZE', 10_000)
+
+    to_process = _get_models()
+    start_date = timezone.now() - timezone.timedelta(days=keep_days)
+
+    for model, history_model in to_process:
+        history_model_manager = history_model.objects
+        history_model_manager = history_model_manager.filter(
+            history_date__lt=start_date
+        ).order_by()
+        found = history_model_manager.count()
+        LOGGER.info(f"{model} has {found} old historical entries")
+        if not found:
+            continue
+        for _ in range(math.ceil(found / chunk_size)):
+            history_model_manager[:chunk_size].delete()
+        LOGGER.info(f"Removed {found} historical records for {model}\n")
