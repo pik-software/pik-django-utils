372ad3116252460e51189dd732720ed2db3169bf
spice0xff <spice0xff@gmail.com>
2022-01-18 20:09:17 +0700

    error import fix

--- pik/cors/models.py
+++ pik/cors/models.py
@@ -1,7 +1,7 @@
 from django.core.cache import caches
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from core.models import BasePHistorical
+from pik.core.models import BasePHistorical
 
 from .consts import DEFAULT_CACHE_NAME, CACHE_KEY
 
