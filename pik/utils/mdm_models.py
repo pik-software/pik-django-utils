5f7a99cad2770dbb2550996bb4ecdc2544f3908e
Николаев Дмитрий Дмитриевич <nikolaevdd@pik.ru>
2022-05-24 05:23:30 +0300

    mdm-models utils: add define_model() and define_serializer() functions to define a class separated from one another. MDM-875

--- pik/utils/mdm_models.py
+++ pik/utils/mdm_models.py
@@ -1,5 +1,4 @@
-import types
-from typing import List, Type, Tuple, Optional, Union
+from typing import Type, Tuple, Optional, Union
 
 from pik.api.lazy_field import LazyField
 
@@ -10,6 +9,19 @@ def _process_mixins_meta(mixin_meta: Type,
         if getattr(mixin_class, 'Meta', None):
             mixin_meta = type('Meta', (mixin_class.Meta, mixin_meta,), attrs)
     return mixin_meta
+
+
+def _process_lazy_fields(new_serializer):
+    """Process all serializer LazyFields and replace its path attributes
+    from BaseSerializers to specific ones"""
+
+    for _, field in \
+            new_serializer._declared_fields.items(): # noqa: protected-access
+        if not isinstance(field, LazyField):
+            continue
+        field._kwargs['path'] = field._kwargs['path'].lstrip('Base') # noqa:
+        # protected-access
+    return new_serializer
 
 
 def define_model(  # noqa: dangerous-default-value
@@ -33,40 +45,6 @@ def define_model(  # noqa: dangerous-default-value
         name, (*mixin_classes, base_model,),
         {**attrs, **excluded_fields, 'Meta': meta})
     variables[name] = new_model
-
-
-def define_models(  # noqa: dangerous-default-value
-    names: List[str],
-    base_module: types.ModuleType,
-    variables: dict,
-    mixins: dict = {},
-) -> None:  # https://github.com/python/mypy/issues/8401
-    """Define Django models dynamically using its names and
-    module with its base models provided"""
-
-    for name in names:
-        base_model = getattr(base_module, f'Base{name}')
-        mixin_classes = mixins.get(name, ())
-        excluded_fields = tuple(
-            field
-            for mixin in mixin_classes
-            for field in getattr(mixin, 'EXCLUDE_PARENT_FIELDS', ())
-        )
-        define_model(base_model, mixin_classes,
-                     variables, name, excluded_fields)
-
-
-def _process_lazy_fields(new_serializer):
-    """Process all serializer LazyFields and replace its path attributes
-    from BaseSerializers to specific ones"""
-
-    for _, field in \
-            new_serializer._declared_fields.items(): # noqa: protected-access
-        if not isinstance(field, LazyField):
-            continue
-        field._kwargs['path'] = field._kwargs['path'].lstrip('Base') # noqa:
-        # protected-access
-    return new_serializer
 
 
 def define_serializer(  # noqa: dangerous-default-value
@@ -94,22 +72,3 @@ def define_serializer(  # noqa: dangerous-default-value
                           {**attrs, **excluded_fields, 'Meta': meta})
     new_serializer = _process_lazy_fields(new_serializer)
     variables[new_serializer_name] = new_serializer
-
-
-def define_serializers(  # noqa: dangerous-default-value
-    names: List[str],
-    base_module: types.ModuleType,
-    variables: dict,
-    model_module: types.ModuleType,
-    mixins: dict = {},
-) -> None:  # https://github.com/python/mypy/issues/8401
-    """Define DRF serializers dynamically using its model names
-    and module with its base serializers provided"""
-
-    for name in names:
-        new_serializer_name = f'{name}Serializer'
-        base_serializer = getattr(base_module, f'Base{new_serializer_name}')
-        model = getattr(model_module, name)
-        mixin_classes = mixins.get(name, ())
-        define_serializer(base_serializer, mixin_classes,
-                          model, variables, name)
