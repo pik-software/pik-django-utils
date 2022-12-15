feaf112f606c7cd653dd45607bea3bdccb56907a
Николаев Дмитрий Дмитриевич <nikolaevdd@pik.ru>
2022-12-15 03:46:54 +0300

    CI/CD: Add models and serializers  by app generation for ds-back. Replace lstrip() to partition() of `str` methods.
    MDM-1168

--- pik/utils/mdm_models.py
+++ pik/utils/mdm_models.py
@@ -1,6 +1,9 @@
+import inspect
+import re
 from typing import Type, Tuple, Optional, Union
 
 from pik.api.lazy_field import LazyField
+from pik.api.viewsets import StandardizedGenericViewSet
 
 
 def _process_mixins_meta(mixin_meta: Type,
@@ -15,12 +18,12 @@ def _process_lazy_fields(new_serializer):
     """Process all serializer LazyFields and replace its path attributes
     from BaseSerializers to specific ones"""
 
-    for _, field in \
-            new_serializer._declared_fields.items(): # noqa: protected-access
+    for _, field in (
+            new_serializer._declared_fields.items()): # noqa: protected-access
         if not isinstance(field, LazyField):
             continue
-        field._kwargs['path'] = field._kwargs['path'].partition('Base')[-1] # noqa:
-        # protected-access
+        field._kwargs['path'] = ( # noqa: protected-access
+            field._kwargs['path'].partition('Base')[-1])
     return new_serializer
 
 
@@ -76,3 +79,47 @@ def define_serializer(  # noqa: dangerous-default-value
                           {**attrs, **excluded_fields, 'Meta': meta})
     new_serializer = _process_lazy_fields(new_serializer)
     variables[new_serializer_name] = new_serializer
+
+
+def define_missing_classes(base_module, variables, base_class):
+    def is_model(item):
+        return (
+            inspect.isclass(item)
+            and item.__module__ == base_module.__name__
+            and issubclass(item, base_class))
+
+    for name, base in inspect.getmembers(base_module, is_model):
+        match = re.match('Base(?P<name>.+)', name)
+        if not match or match['name'] in variables:
+            continue
+
+        variables[match['name']] = type(
+            match['name'], (base, ), {'__module__': variables['__name__']})
+
+
+def get_module_classes(module, predicate=None):
+    return {
+        name: item
+        for name, item in inspect.getmembers(module, predicate)
+        if item.__module__ == module.__name__
+    }
+
+
+def define_missing_viewsets(variables, base_viewsets, serializers, filters):
+    predicate = lambda x: inspect.isclass(x) and issubclass(  # noqa: lambda
+        x, StandardizedGenericViewSet)
+    definitions = get_module_classes(base_viewsets, predicate)
+    for base_name, base in definitions.items():
+        match = re.match('Base(?P<name>.+)ViewSet', base_name)
+        if not match:
+            continue
+        serializer_name = f'{match["name"]}Serializer'
+        viewset_name = f'{match["name"]}ViewSet'
+        filter_name = f'{match["name"]}Filter'
+        if viewset_name not in variables:
+            variables[viewset_name] = type(
+                viewset_name, (base, ), {
+                    '__module__': variables['__name__'],
+                    'serializer_class': getattr(serializers, serializer_name),
+                    'filterset_class': getattr(filters, filter_name)
+                })
