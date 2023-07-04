c51c286a6e8e2d836768486dbcf3a422255d1d4c
spice0xff <spice0xff@gmail.com>
2023-07-04 12:49:14 +0700

    tests fix

--- tests/test_api_validators.py
+++ tests/test_api_validators.py
@@ -53,7 +53,11 @@ class TestNewestUpdateValidator:
 
         with pytest.raises(ValidationError) as error:
             serializer.is_valid(raise_exception=True)
-        expected = ({'updated': [ErrorDetail(
+        # expected = ({'updated': [ErrorDetail(
+        #     string='Новое значене поля updated должно быть больше '
+        #            'предыдущего.',
+        #     code='newest_update_validation_error')]}, )
+        expected = ({'non_field_errors': [ErrorDetail(
             string='Новое значене поля updated должно быть больше '
                    'предыдущего.',
             code='newest_update_validation_error')]}, )
