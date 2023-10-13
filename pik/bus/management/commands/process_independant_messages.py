44f56d450459cf4ef0e85423d7865ffb7a718fd7
spice0xff <spice0xff@gmail.com>
2023-10-13 08:50:25 +0700

    refactoring

--- pik/bus/management/commands/process_independant_messages.py
+++ pik/bus/management/commands/process_independant_messages.py
@@ -11,9 +11,9 @@ from pik.bus.mdm import mdm_event_captor
 from pik.utils.sentry import capture_exception
 
 
+LOCK_KEY = 'bus-process-independant-message'
+LOCK_TIMEOUT = 24 * 60 * 60
 CHUNK_SIZE = 2**10
-LOCK_ID = 'process-independant-message'
-LOCK_TIMEOUT = 60 * 60
 
 logger = logging.getLogger(__name__)
 handler_class = import_string(getattr(
@@ -25,17 +25,20 @@ class Command(BaseCommand):
     help = 'Processing independent PIKMessageException errors'
 
     def handle(self, *args, **options):
-        logger.info(self.help)
-        qs = PIKMessageException.objects.filter(has_dependencies=False)
         try:
-            lock = cache.lock(f'bus-{LOCK_ID}', timeout=LOCK_TIMEOUT)
-            with lock:
-                with tqdm(total=qs.count()) as pbar:
-                    for obj in qs.iterator(chunk_size=CHUNK_SIZE):
-                        handler = handler_class(
-                            obj.message, obj.queue, mdm_event_captor)
-                        handler.handle()
-                        pbar.update(1)
-
+            self._run_process()
         except Exception as exc:  # noqa: broad-except
             capture_exception(exc)
+
+    def _run_process(self):
+        logger.info(self.help)
+        with cache.lock(LOCK_KEY, timeout=LOCK_TIMEOUT, blocking=False) as lock:
+            if not lock:
+                return
+            self._process()
+
+    @staticmethod
+    def _process():
+        qs = PIKMessageException.objects.filter(has_dependencies=False)
+        for obj in tqdm(qs.iterator(chunk_size=CHUNK_SIZE), total=qs.count()):
+            handler_class(obj.message, obj.queue, mdm_event_captor).handle()
