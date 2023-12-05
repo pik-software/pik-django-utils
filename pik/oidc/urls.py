01875b94fa2380bf5f701c3db6cd7c6fe16a481b
spice0xff <spice0xff@gmail.com>
2023-12-05 13:00:37 +0700

    fix prospector messages

--- pik/oidc/urls.py
+++ pik/oidc/urls.py
@@ -1,21 +1,20 @@
-from django.conf.urls import url, include
+from django.conf.urls import include, re_path
 from django.contrib import admin
 
-from .views import (oidc_admin_login, oidc_admin_logout,
-                    oidc_backchannel_logout, complete)
+from .views import (
+    oidc_admin_login, oidc_admin_logout, oidc_backchannel_logout, complete)
 
 
 urlpatterns = [  # noqa: invalid-name
-
     # We need to override default `social_python` `complete` behavior in order
     # to provide backchannel logout implementation.
-    url(r'^openid/complete/(?P<backend>[^/]+)/', complete),
-    url(r'^openid/logout/(?P<backend>[^/]+)/$',
+    re_path(r'^openid/complete/(?P<backend>[^/]+)/', complete),
+    re_path(r'^openid/logout/(?P<backend>[^/]+)/$',
         oidc_backchannel_logout, name='oidc_backchannel_logout'),
 
-    url(r'^openid/', include('social_django.urls', namespace='social')),
-    url(r'^login/$', admin.site.login, name='login'),
-    url(r'^logout/$', admin.site.logout, name='logout'),
-    url(r'^admin/login/$', oidc_admin_login),
-    url(r'^admin/logout/$', oidc_admin_logout),
+    re_path(r'^openid/', include('social_django.urls', namespace='social')),
+    re_path(r'^login/$', admin.site.login, name='login'),
+    re_path(r'^logout/$', admin.site.logout, name='logout'),
+    re_path(r'^admin/login/$', oidc_admin_login),
+    re_path(r'^admin/logout/$', oidc_admin_logout)
 ]
